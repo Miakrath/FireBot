b797b7bcbdf1d44af8931cd72c33d9ad60ebe0fb
Miakrath <39339747+Miakrath@users.noreply.github.com>
2018-05-16 17:01:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ("*")
 
 bot.on('ready', function() {
-    bot.user.setGame("command: *help")
+    bot.user.setActivity("command: *help")
     console.log("connected");
 });
 
@@ -13,13 +13,20 @@ bot.login("NDQ2MzAwNjEyNTQ0NDMwMDgw.Dd3CKQ.jqFMAGvr-Js_DdjIAAXXkWXykX0")
 
 bot.on('message', message => {
     if(message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n -*help");
-
+        message.channel.send("Liste des commandes: \n *help \n *combat \n *calendrier \n ");
     }
-
     if (message.content === "Salut"){
         message.reply("Bien le bonjour. :)");
         console.log("Commande Salut effectué");
     }
+    if (message.content === prefix + "combat"){
+        message.reply("Les combats prévus dans cette semaine sont : *Joueur* VS *Joueur* !")
+    }
+    if (message.content === prefix + "calendrier"){
+        message.reply("Voici le calendrier des combats !")
+
+    }
+
+
 });
 
